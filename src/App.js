396d6019c55d13bc5c6fede2bdd0600191a6c45c
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-04 23:46:35 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 //  Libraries
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
-import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
@@ -11,10 +10,6 @@ import SignIn from './components/SignIn/SignIn';
 import Register from './components/Register/Register';
 //  CSS
 import './App.css';
-
-const app = new Clarifai.App({
-    apiKey: '03e0e56a08d94023adbfd0fdd7ab27ce'
-});
 
 const particlesOptions={
     particles: {
@@ -95,9 +90,13 @@ class App extends Component {
             imageUrl: this.state.input
         });
 
-        app.models.predict(
-            Clarifai.FACE_DETECT_MODEL, 
-            this.state.input)
+        fetch('http://localhost:3000/imageurl', {
+            method: 'post',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                input: this.state.input
+            })
+        }).then(response => response.json())
             .then(response => {
                 if(response){
                     fetch('http://localhost:3000/image', {
@@ -109,18 +108,16 @@ class App extends Component {
                     })
                     .then(response => response.json())
                     .then(count => {
-                        this.setState(Object.assign(this.state.user, { entries: count}))
+                        this.setState(Object.assign(this.state.user, { entries: count }))
                     })
                     .catch(console.log)
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))})
-            .catch(err => console.log(err));
+            .catch(err => console.log(err))
     }
     
     render(){
-
-       const {isSignedIn, imageUrl, box, route} = this.state;
-       
+       const {isSignedIn, imageUrl, box, route} = this.state;    
        return (
           <div className="App">
             <Particles 
