03504be68940e8f0f0e094b2827275e4e11cb63d
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-04 22:17:07 +0100

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -32,6 +32,7 @@ class SignIn extends React.Component {
        .then(response => response.json())
        .then(user => {
            if(user.id){
+               console.log(user);
             this.props.loadUser(user);
             this.props.onRouteChange('home');
            }
