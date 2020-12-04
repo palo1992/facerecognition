7c83538905c2ba9419b651ce5ea88db0357c6229
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-04 22:31:31 +0100

    update

--- src/App.js
+++ src/App.js
@@ -27,24 +27,24 @@ const particlesOptions={
         }
     }
 }
-
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box:{},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+        id: '',
+        name: '',
+        email: '',
+        entries: 0,
+        joined: ''
+    }
+}
 class App extends Component {
     constructor(){
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box:{},
-            route: 'signin',
-            isSignedIn: false,
-            user: {
-                id: '',
-                name: '',
-                email: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
 
     loadUser = (data) => {
@@ -83,7 +83,7 @@ class App extends Component {
     }
     onRouteChange = (route) =>{
         if(route === 'signout'){
-            this.setState({isSignedIn: false})
+            this.setState(initialState)
         } else if(route === 'home'){
             this.setState({isSignedIn: true})
         }
@@ -111,6 +111,7 @@ class App extends Component {
                     .then(count => {
                         this.setState(Object.assign(this.state.user, { entries: count}))
                     })
+                    .catch(console.log)
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))})
             .catch(err => console.log(err));
