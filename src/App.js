92e1b4c857ee70def539defbc46e0162dad15dd4
palo1992 <65390185+palo1992@users.noreply.github.com>
2020-12-05 18:20:50 +0100

    update for heroku

--- src/App.js
+++ src/App.js
@@ -90,7 +90,7 @@ class App extends Component {
             imageUrl: this.state.input
         });
 
-        fetch('http://localhost:3000/imageurl', {
+        fetch('https://smart-brain-facerecognition-ap.herokuapp.com//imageurl', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
@@ -99,7 +99,7 @@ class App extends Component {
         }).then(response => response.json())
             .then(response => {
                 if(response){
-                    fetch('http://localhost:3000/image', {
+                    fetch('https://smart-brain-facerecognition-ap.herokuapp.com//image', {
                         method: 'put',
                         headers: {'Content-Type': 'application/json'},
                         body: JSON.stringify({
